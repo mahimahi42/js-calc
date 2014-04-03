835c81823240559c5bf6eb4b9bd7ea12de7d57c3
Bryce Davis <bad1128@gmail.com>
2014-04-03 09:48:20 -0400

    Implement multiply, subtract, and divide methods

--- calc.js
+++ calc.js
@@ -1,1 +1,12 @@
 var add = function(x, y) { return x + y; };
+
+var multiply = function(x, y) { return x * y; };
+
+var subtract = function(x, y) { return x - y; };
+
+var divide = function(x, y) {
+	if (y === 0) {
+		return NaN;
+	}
+	return x / y;
+};
